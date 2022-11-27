37074f5916248690485c94a5f9a25d128ed1d7cc
guoruiming <guoruiming@stu.scu.edu.cn>
2022-11-27 11:12:03 +0800

    talk: finish

---
+++ theme/settings.lua
@@ -0,0 +1,23 @@
+local Utils = require("theme/Utils")
+local config = require("theme/config")
+
+local baseConfig = Utils.checkColors(config)
+
+print(baseConfig)
+
+return {
+    PrimaryColor            = baseConfig.PrimaryColor;
+    PrimaryColorLight       = baseConfig.PrimaryColorLight;
+    PrimaryColorDark        = baseConfig.PrimaryColorDark;
+    PrimaryTextColor        = baseConfig.PrimaryTextColor;
+    SecondaryColor          = baseConfig.SecondaryColor;
+    SecondaryColorLight     = baseConfig.SecondaryColorLight;
+    SecondaryColorDark      = baseConfig.SecondaryColorDark;
+    SecondaryTextColor      = baseConfig.SecondaryTextColor;
+    AccentColor             = baseConfig.AccentColor;
+    Shadow                  = baseConfig.Shadow;
+    BackgroundColor1        = baseConfig.BackgroundColor1;
+    BackgroundColor2        = baseConfig.BackgroundColor1;
+    BackgroundColor3        = baseConfig.BackgroundColor3;
+    BackgroundColor4        = baseConfig.BackgroundColor3;
+}
